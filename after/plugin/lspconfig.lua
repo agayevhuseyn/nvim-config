facaa19951b196ed8ac492f0d7739c361f6a216b
Huseyn Aghayev <huseyn@seallang.org>
2025-07-07 11:49:34 +0400

    comment lsps

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -11,18 +11,10 @@ vim.diagnostic.config({
 
 local lspconfig = require('lspconfig')
 
-lspconfig.clangd.setup({})
-lspconfig.lua_ls.setup({})
+--lspconfig.clangd.setup({})
+--lspconfig.lua_ls.setup({})
 --lspconfig.ruby_lsp.setup({})
-lspconfig.pylsp.setup({})
+--lspconfig.pylsp.setup({})
 --lspconfig.elixirls.setup({
 --    cmd = {"/home/huseyn/.local/share/nvim/mason/packages/elixir-ls/language_server.sh"}
 --})
-
-lspconfig.elixirls.setup {
-  cmd = { "/home/huseyn/.local/share/nvim/mason/packages/elixir-ls/_build/shared/rel/language_server/bin/language_server" },
-  capabilities = capabilities,
-  on_attach = function(client, bufnr)
-    print("ElixirLS attached!")  -- this should print if it's working
-  end,
-}
