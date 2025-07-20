ef06d1d2c0085b4fa455ecb874967ef6fc54d891
Huseyn Aghayev <huseyn@seallang.org>
2025-07-20 20:44:52 +0400

    copilot disable by default & enable with keymap

--- init.lua
+++ init.lua
@@ -16,3 +16,20 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.expandtab = true
   end,
 })
+
+-- Copilot
+vim.g.copilot_enabled = false
+
+local function toggle_copilot()
+    if vim.g.copilot_enabled then
+        vim.cmd("Copilot disable")
+        vim.g.copilot_enabled = false
+        print("Copilot disabled")
+    else
+        vim.cmd("Copilot enable")
+        vim.g.copilot_enabled = true 
+        print("Copilot enabled")
+    end
+end
+
+vim.keymap.set("n", "<leader>cc", toggle_copilot)
